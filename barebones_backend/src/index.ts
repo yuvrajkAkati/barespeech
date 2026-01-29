e8333be89cab1148b7528bb1a2dd64b2cbb32185
yuvrajkAkati <139755961+yuvrajkAkati@users.noreply.github.com>
2026-01-29 19:53:04 +0530

    context feeding

---
+++ barebones_backend/src/index.ts
@@ -0,0 +1,11 @@
+import express from "express"
+const app = express();
+app.use(express.json());
+
+app.get("/health", (_, res) => {
+  res.send("OK");
+});
+
+app.listen(3001, () => {
+  console.log("Backend running on port 3001");
+});
